ddfcc546c40297d52cce3418a7bf51b84f480c9f
unknown <joshua.a.disney@gmail.com>
2023-03-30 22:24:46 -0700

    Create search functionality and add todo list

--- src/components/pokeApi.js
+++ src/components/pokeApi.js
@@ -2,22 +2,33 @@ import React, { useState, useEffect } from 'react'
 
 export const PokeApi = () => {
 
+    const [pokeName, setPokeName] = useState('')
     const [pokeInfo, setPokeInfo] = useState({})
 
-    useEffect(() => {
-        const getPokemon = async () => {
-            const result = await fetch('https://pokeapi.co/api/v2/pokemon/pidgeotto')
-            const myJson = await result.json()   
-            setPokeInfo(myJson)
-        }
-        
-        getPokemon()
-    }, [])
+    const getPokemon = async (name) => {
+        const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        const myJson = await result.json()   
+        setPokeInfo(myJson)
+    }
 
    return(
     <div>
-        <p>This should display information about {pokeInfo.name ? pokeInfo.name : 'a pokemon'}</p>
-        {pokeInfo.moves ? pokeInfo.moves.slice(0, 4).map((move) => <p key={move.move.name}>{move.move.name}</p>) : <p>....Loading</p>}
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            getPokemon(pokeName)}}>
+            <input 
+                type='text' 
+                name='pokeName' 
+                id='pokeName'
+                placeholder='...type a Pokemon name' 
+                value={pokeName} 
+                onChange={(e) => setPokeName(e.target.value)} 
+            />
+        </form>
+        <section>
+            <h2>This should display information about {pokeName ? pokeName : 'a pokemon'}</h2>
+            {pokeInfo.moves ? pokeInfo.moves.slice(0, 4).map((move) => <p key={move.move.name}>{move.move.name}</p>) : <p>....Loading</p>}
+        </section>
     </div>
    )
 }
